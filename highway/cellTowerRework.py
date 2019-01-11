20b7ac907bd7a8d711cdb93366b8303045a85dde
Stefan Körner <stefankoerner@outlook.com>
2019-01-11 18:24:23 +0100

    Reorderd

--- highway/cellTowerRework.py
+++ highway/cellTowerRework.py
@@ -8,26 +8,25 @@ print("importing file : "+data_in)
 df = pandas.read_csv(data_in, usecols=cols_to_get, na_filter=False)
 
 # Celltowers on the west coast
-locx = df.loc[df['X'] < -117]
+df = df.loc[df['X'] < -117]
 
-
-minix = df['X'].min()
-miniy = df['Y'].min()
-df['X'] = df['X'] - minix
-df['Y'] = df['Y'] - miniy
-
-
-print("Casting coordinates into integers")
-df['X'] = df['X'].astype(int)
-df['Y'] = df['Y'].astype(int)
-
-#Casting from Lat Lon to NAD83
-
+print("Casting from Lat Lon to NAD83")
 for index, row in df.iterrows():
     g = latlong2grid(row["X"], row["Y"], tag = 'NAD83')
     #print(g.E,g.N)
-    df.at[index, 'X'] = g.E
-    df.at[index, 'Y'] = g.N
+    df.at[index, 'X'] = g.N
+    df.at[index, 'Y'] = g.E
+
+# Hardcoded values from the highway dataset (11 jan 2019)
+# minix = 6451063.948 #df['X'].min()
+# miniy = 1871874.94 #df['Y'].min()
+# df['X'] = df['X'] - minix
+# df['Y'] = df['Y'] - miniy  
+
+print("Casting coordinates into integers")
+df['X'] = df['X'].astype(int)
+df['Y'] = df['Y'].astype(int)    
+
 
 data_out = "cellTowersWestCoast_fixed.csv"
 cols_to_out = ['X', 'Y', 'STRUCTYPE']
