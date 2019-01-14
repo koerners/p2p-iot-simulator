22c8b07c169da7d8acf86fa2bba800bbfeaf8107
Stefan Körner <stefankoerner@outlook.com>
2019-01-14 18:23:38 +0100

    Still working on dataset

--- highway/cellTowerRework.py
+++ highway/cellTowerRework.py
@@ -1,6 +1,6 @@
 import pandas
 from OSGridConverter import latlong2grid
-
+import pyproj
 #needed columns
 cols_to_get = ['X', 'Y', 'STRUCTYPE']
 data_in = "Cellular_Towers.csv"
@@ -11,12 +11,20 @@ df = pandas.read_csv(data_in, usecols=cols_to_get, na_filter=False)
 df = df.loc[df['X'] < -117]
 df = df.loc[df['X'] > -127]
 
-# print("Casting from Lat Lon to NAD83")
-# for index, row in df.iterrows():
-#     g = latlong2grid(row["X"], row["Y"], tag = 'NAD83')
-#     #print(g.E,g.N)
-#     df.at[index, 'X'] = g.N
-#     df.at[index, 'Y'] = g.E
+p1 = pyproj.Proj(proj='latlong',datum='WGS84')
+p2 = pyproj.Proj(init='epsg:2227')
+
+print("Casting from Lat Lon to NAD83")
+for index, row in df.iterrows():
+    x1 = row["X"]; y1 = row["Y"]
+    x2, y2 = pyproj.transform(p1, p2, x1, y1)
+    df.at[index, 'X'] = x2
+    df.at[index, 'Y'] = y2
+    print(x2, y2)
+
+
+
+
 
 # Hardcoded values from the highway dataset (11 jan 2019)
 # minix = 6451063.948 #df['X'].min()
@@ -27,6 +35,8 @@ df = df.loc[df['X'] > -127]
 # print("Casting coordinates into integers")
 # df['X'] = df['X'].astype(int)
 # df['Y'] = df['Y'].astype(int)    
+
+
 
 
 data_out = "cellTowersWestCoast_fixed.csv"
